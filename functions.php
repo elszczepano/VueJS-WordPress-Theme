33fce169ecc587b6b49bdb77fbe39fbb7b57ed5e
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-05-15 15:18:19 +0200

    Create required project template files

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+// Remove all default WP template redirects/lookups
+remove_action('template_redirect', 'redirect_canonical');
+// Redirect all requests to index.php so the Vue app is loaded and 404s aren't thrown
+function remove_redirects() {
+    add_rewrite_rule('^/(.+)/?', 'index.php', 'top');
+}
+add_action('init', 'remove_redirects');
+// Load scripts
+function load_vue_scripts() {
+    //wp_enqueue_style('blankslate/app.css', get_template_directory_uri() . '/dist/styles/app.css', false, null);
+    wp_enqueue_script('app.js', get_template_directory_uri() . '/dist/js/bundle.js', null, null, true);
+}
+add_action('wp_enqueue_scripts', 'load_vue_scripts', 100);
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php status_header(200); ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> class="no-js no-svg">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <?php wp_head(); ?>
+</head>
+<body>
+<div id="app"></div>
+<?php wp_footer(); ?>
+</body>
+</html>

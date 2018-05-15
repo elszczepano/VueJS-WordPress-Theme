5ca715ff2d529c5f532a233780182137f73706d0
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-05-15 15:37:26 +0200

    Update eslint rules

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -14,7 +14,7 @@ const config = {
     mode: 'development',
     module: {
         rules: [
-            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' },
             { test: /\.(scss|sass)$/,
                 use: ExtractTextPlugin.extract({
                     fallback: 'style-loader',

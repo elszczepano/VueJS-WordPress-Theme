94979d8327fbd6f8c6cd95b68c1fe0e4a28e7d73
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-05-15 15:17:21 +0200

    Update project config

---
+++ webpack.config.babel.js
@@ -0,0 +1,64 @@
+import path from 'path';
+import ExtractTextPlugin from 'extract-text-webpack-plugin';
+import OptimizeCssAssetsPlugin from 'optimize-css-assets-webpack-plugin';
+import VueLoaderPlugin from 'vue-loader/lib/plugin';
+
+const config = {
+  entry: {
+      bundle: './src/main.js'
+  },
+    output: {
+        filename: 'js/[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            { test: /\.(scss|sass)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'autoprefixer-loader' ,'sass-loader']
+                })
+            },
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader',
+                options: {
+                    loaders: {
+                        js: 'babel-loader'
+                    }
+                }
+            },
+            {
+                test: /\.(png|jpg|gif)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {}
+                    }
+                ]
+            }
+        ]
+    },
+    resolve: {
+        alias: {
+            'vue$': 'vue/dist/vue.esm.js'
+        },
+        extensions: ['*', '.js', '.vue', '.json']
+    },
+    plugins: [
+        new ExtractTextPlugin({
+            filename: 'style.css'
+        }),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.optimize\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: { discardComments: { removeAll: true } },
+            canPrint: true
+        }),
+        new VueLoaderPlugin()
+    ]
+};
+
+module.exports = config;

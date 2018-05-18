a2060cefc70ef2b2e71164cdf8036347a11aee06
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-05-18 20:21:13 +0200

    Setup logo font and material icons

--- functions.php
+++ functions.php
@@ -1,4 +1,6 @@
 <?php
+show_admin_bar(false);
+
 remove_action('template_redirect', 'redirect_canonical');
 
 function remove_redirects() {
@@ -9,6 +11,7 @@ add_action('init', 'remove_redirects');
 function load_vue_scripts() {
 
     wp_enqueue_style('roboto', 'https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i&amp;subset=latin-ext', false, null);
+    wp_enqueue_style('icons', 'https://fonts.googleapis.com/icon?family=Material+Icons', false, null);
     wp_enqueue_style('style', get_template_directory_uri() . '/dist/style.css', false, null);
     wp_enqueue_script('app.js', get_template_directory_uri() . '/dist/js/bundle.js', null, null, true);
 }
--- src/Components/MainHeader.vue
+++ src/Components/MainHeader.vue
@@ -1,17 +1,33 @@
 <template>
     <v-app>
-        <v-content>
-            <v-container>Hello world</v-container>
-        </v-content>
+        <v-toolbar>
+            <v-toolbar-side-icon></v-toolbar-side-icon>
+            <v-toolbar-title class="logo">devszczepaniak.pl</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn flat>Link One</v-btn>
+                <v-btn flat>Link Two</v-btn>
+                <v-btn flat>Link Three</v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
     </v-app>
 </template>
 
 <script>
     export default {
-        name: "main-header"
+        name: "main-header",
+        data: () => ({
+            drawer: null
+        }),
+        props: {
+            source: String
+        }
     }
 </script>
 
-<style scoped>
-
+<style lang="scss" scoped>
+    @import url('https://fonts.googleapis.com/css?family=Quicksand');
+    .logo {
+        font-family: 'Quicksand', sans-serif;
+    }
 </style>

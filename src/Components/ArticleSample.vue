a12e1f2bcd8774727bf56efba734bf302865a623
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-05-20 22:12:04 +0200

    Separate v-card to another component

---
+++ src/Components/ArticleSample.vue
@@ -0,0 +1,25 @@
+<template>
+    <v-card>
+        <v-card-media src="https://www.w3schools.com/html/img_girl.jpg" height="200px">
+        </v-card-media>
+        <v-card-title primary-title>
+            <div>
+                <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
+                <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+            </div>
+        </v-card-title>
+        <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn flat>Czytaj dalej</v-btn>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: 'article-sample'
+    };
+</script>
+
+<style scoped>
+</style>
--- src/Components/NewestArticles.vue
+++ src/Components/NewestArticles.vue
@@ -2,55 +2,29 @@
     <v-container grid-list-md>
         <h2 class="display-2 main-page--header"><span class="text__red">N</span>ajnowsze artykuły</h2>
         <v-layout row wrap>
-            <v-flex d-flex xs12 md7>
+            <v-flex d-flex xs12 md8>
                 <v-card>
-                    <v-card-media src="https://www.w3schools.com/html/img_girl.jpg" height="200px">
+                    <v-card-media src="https://www.w3schools.com/html/img_girl.jpg" height="575px">
                     </v-card-media>
                     <v-card-title primary-title>
                         <div>
                             <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-                            <div>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aperiam consequatur esse est, hic ipsum iste quis quisquam recusandae similique voluptatem! Ab, doloribus eveniet ex iste laborum praesentium quia sequi voluptates.Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
+                            <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
                         </div>
                     </v-card-title>
                     <v-card-actions>
-                        <v-btn flat color="orange">Share</v-btn>
-                        <v-btn flat color="orange">Explore</v-btn>
+                        <v-spacer></v-spacer>
+                        <v-btn flat>Czytaj dalej</v-btn>
                     </v-card-actions>
                 </v-card>
             </v-flex>
-            <v-flex d-flex xs12 md5>
+            <v-flex d-flex xs12 md4>
                 <v-layout row wrap>
                     <v-flex d-flex>
-                        <v-card>
-                            <v-card-media src="https://www.w3schools.com/html/img_girl.jpg" height="200px">
-                            </v-card-media>
-                            <v-card-title primary-title>
-                                <div>
-                                    <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-                                    <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
-                                </div>
-                            </v-card-title>
-                            <v-card-actions>
-                                <v-btn flat color="orange">Share</v-btn>
-                                <v-btn flat color="orange">Explore</v-btn>
-                            </v-card-actions>
-                        </v-card>
+                        <article-sample />
                     </v-flex>
                     <v-flex d-flex>
-                        <v-card>
-                            <v-card-media src="https://www.w3schools.com/html/img_girl.jpg" height="200px">
-                            </v-card-media>
-                            <v-card-title primary-title>
-                                <div>
-                                    <h3 class="headline mb-0">Kangaroo Valley Safari</h3>
-                                    <div>Located two hours south of Sydney in the <br>Southern Highlands of New South Wales, ...</div>
-                                </div>
-                            </v-card-title>
-                            <v-card-actions>
-                                <v-btn flat color="orange">Share</v-btn>
-                                <v-btn flat color="orange">Explore</v-btn>
-                            </v-card-actions>
-                        </v-card>
+                        <article-sample />
                     </v-flex>
                 </v-layout>
             </v-flex>
@@ -59,7 +33,9 @@
 </template>
 
 <script>
+    import ArticleSample from './ArticleSample';
     export default {
+        components: {ArticleSample},
         name: 'newest-articles'
     };
 </script>

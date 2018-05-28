72af83dbf6d6764afc246769f4026cb09967a0db
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-05-28 12:30:01 +0200

    Infinite scrolling on search page

--- src/Components/Search.vue
+++ src/Components/Search.vue
@@ -8,9 +8,12 @@
                     <span class="text__red">{{this.$route.query.s}}</span>
                 </h2>
                 <article-list-sample :details="articles[index]" v-for="(article, index) in articles" :key="`${index}`" />
-                <h3 class="text-xs-center" v-show="!articles.length">Brak wyników wyszukiwania...</h3>
+                <infinite-loading force-use-infinite-wrapper="true" @infinite="infiniteHandler">
+                    <span v-show="articles.length>10" class="headline" slot="no-more">Koniec artykułów...</span>
+                    <h3 v-show="!articles.length" class="text-xs-center">Brak wyników wyszukiwania...</h3>
+                </infinite-loading>
             </v-flex>
-        </v-layout>
+        </v-layout>a
         <main-footer />
     </v-app>
 </template>
@@ -19,30 +22,44 @@
     import MainHeader from './MainHeader';
     import MainFooter from './MainFooter';
     import ArticleListSample from './ArticleListSample';
+    import InfiniteLoading from 'vue-infinite-loading';
     import API from '../api';
     export default {
         components: {
             ArticleListSample,
             MainFooter,
-            MainHeader
+            MainHeader,
+            InfiniteLoading
         },
         name: 'search',
         data: () => ({
-            articles: []
+            articles: [],
+            postsCount: 0
         }),
         methods: {
-            loadPosts() {
-                API.get(`posts?search=${this.$route.query.s}`)
-                    .then(response => this.articles = response['data'])
+            loadPosts(param) {
+                API.get(`posts?search=${this.$route.query.s}&per_page=${param}`)
+                    .then(response => {
+                        this.postsCount = parseInt(response.headers['x-wp-total'], 10);
+                        this.articles = response['data'];
+                    });
+            },
+            infiniteHandler($state) {
+                setTimeout(() => {
+                    const articles = this.articles.length;
+                    this.loadPosts(articles + 10);
+                    if(this.articles.length === this.postsCount) $state.complete();
+                    $state.loaded();
+                }, 500);
             }
         },
         watch: {
             '$route' () {
-                this.loadPosts();
+                this.loadPosts(10);
             }
         },
         mounted() {
-            this.loadPosts();
+            this.loadPosts(10);
         }
     };
 </script>

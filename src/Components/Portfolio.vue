e474e46c90b6cd867e2cbda911b61bb1d5a4b7fc
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-05-28 13:18:43 +0200

    Displaying portfolio page

--- src/Components/Portfolio.vue
+++ src/Components/Portfolio.vue
@@ -2,7 +2,14 @@
     <v-app>
         <main-header/>
         <v-layout class="mx-auto default--container">
-
+            <v-container grid-list-md mb-5>
+                <h2 class="display-2 text-xs-center main-page--header">Portfolio</h2>
+                <v-layout row wrap>
+                    <v-flex v-for="(project, index) in projects" :key="`${index}`" xs12 sm6>
+                        <portfolio-sample :details="project"/>
+                    </v-flex>
+                </v-layout>
+            </v-container>
         </v-layout>
         <main-footer/>
     </v-app>
@@ -11,14 +18,24 @@
 <script>
     import MainHeader from './MainHeader';
     import MainFooter from './MainFooter';
-
+    import PortfolioSample from './PortfolioSample';
+    import API from '../api';
     export default {
         components: {
+            PortfolioSample,
             MainFooter,
             MainHeader
         },
-        name: 'portfolio'
+        name: 'portfolio',
+        data: () => ({
+            projects: []
+        }),
+        mounted() {
+            API.get('projects?per_page=100')
+                .then(response => this.projects = response['data'])
+        }
     };
+
 </script>
 
 <style scoped>
---
+++ src/Components/PortfolioSample.vue
@@ -0,0 +1,50 @@
+<template>
+    <v-card>
+        <v-card-media :src="thumbnail" height="200px"></v-card-media>
+        <v-card-title primary-title>
+            <div>
+                <h3 class="headline mb-2">{{title}}</h3>
+                <div v-html="description"></div>
+            </div>
+        </v-card-title>
+        <v-card-actions>
+                <v-layout row wrap>
+                    <v-flex v-if="source" xs12 d-flex justify-center>
+                        <v-icon>fab fa-github</v-icon>
+                        <v-btn flat :href="source">Źródło</v-btn>
+                    </v-flex>
+                    <v-flex v-if="preview" xs12 d-flex justify-center>
+                        <v-icon>fas fa-link</v-icon>
+                        <v-btn flat :href="preview">Demo</v-btn>
+                    </v-flex>
+                </v-layout>
+        </v-card-actions>
+    </v-card>
+</template>
+
+<script>
+    export default {
+        name: 'article-list-sample',
+        props: [
+            'details'
+        ],
+        data: () => ({
+            thumbnail: '',
+            title: '',
+            description: '',
+            source: '',
+            preview: ''
+        }),
+        created() {
+            this.title = this.details['title']['rendered'];
+            this.thumbnail = this.details['better_featured_image']['source_url'];
+            this.description = this.details['content']['rendered'];
+            this.source = this.details['acf']['source'];
+            this.preview = this.details['acf']['preview'];
+        }
+    };
+</script>
+
+<style scoped>
+
+</style>

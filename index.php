2d8662f24be35db0a592455aab208ad535f23885
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-05-29 16:52:39 +0200

    Add title

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta charset="<?php bloginfo( 'charset' ); ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
     <?php wp_head(); ?>
+    <title><?php wp_title(); ?></title>
 </head>
 <body>
 <div id="app"></div>

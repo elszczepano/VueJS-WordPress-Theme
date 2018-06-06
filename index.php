065cd061eb1c021607de7cf3e7d34ffda394ed11
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-06-06 10:42:18 +0200

    Add tab color on Chrome mobile

--- index.php
+++ index.php
@@ -3,6 +3,7 @@
 <html <?php language_attributes(); ?> class="no-js no-svg">
 <head>
     <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="theme-color" content="#263238">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
     <?php wp_head(); ?>
     <title><?php if (is_front_page()) bloginfo( 'name' );  else  wp_title();?></title>

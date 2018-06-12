e0f10a880b8bd4c745582cc4c3085f367723f259
Dominik Szczepaniak <szczepaniak.dominik98@gmail.com>
2018-06-12 21:47:33 +0200

    Mute jquery

--- functions.php
+++ functions.php
@@ -13,6 +13,31 @@ add_filter( 'acf/rest_api/field_settings/show_in_rest', '__return_true' );
 
 show_admin_bar(false);
 
+//mute jquery migrate
+
+function jquery_migrate_silencer() {
+    // create function copy
+    $silencer = '<script>window.console.logger = window.console.log; ';
+    // modify original function to filter and use function copy
+    $silencer .= 'window.console.log = function(tolog) {';
+    // bug out if empty to prevent error
+    $silencer .= 'if (tolog == null) {return;} ';
+    // filter messages containing string
+    $silencer .= 'if (tolog.indexOf("Migrate is installed") == -1) {';
+    $silencer .= 'console.logger(tolog);} ';
+    $silencer .= '}</script>';
+    return $silencer;
+}
+
+add_filter('script_loader_tag','jquery_migrate_load_silencer', 10, 2);
+function jquery_migrate_load_silencer($tag, $handle) {
+    if ($handle == 'jquery-migrate') {
+        $silencer = jquery_migrate_silencer();
+        // prepend to jquery migrate loading
+        $tag = $silencer.$tag;
+    }
+    return $tag;
+}
 //disable jquery
 
 wp_deregister_script('jquery');
